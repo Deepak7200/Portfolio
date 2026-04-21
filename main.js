3482c9cf4c4897689d80800e74dd114fb69d9ce1
Deepak7200 <mail.deepaksehrawat@gmail.com>
2026-04-21 16:05:12 +0530

    Initial commit

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 var typed = new Typed(".text", {
-    strings: ["Problem Solver", "MERN Stack Developer", "Coder"], // Note the corrected property name "Strings" to "strings"
+    strings: ["Problem Solver", "MERN Stack Developer", "Competitive Programmer"], // Note the corrected property name "Strings" to "strings"
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
     loop: true
 });
+
+
